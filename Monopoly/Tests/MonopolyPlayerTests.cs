073895cbaf5046b25f28291b26a793aa8d961fc9
92VoronchikhinAE <VoronchikhinAE01@cbr.ru>
2021-05-12 16:30:14 +0300

    Добавил пару тестов для MonopolyPlayer

---
+++ Monopoly/Tests/MonopolyPlayerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Monopoly
+{
+    [TestFixture]
+    class MonopolyPlayerTests
+    {
+        [Test]
+        public void CreatePlayerWithDefaultAmountCorrectName ()
+        {
+            MonopolyPlayer player = new MonopolyPlayer( "Peter" );
+            Assert.AreEqual( "Peter", player.Name );
+        }
+        [Test]
+        public void CreatePlayerWithDefaultAmountCorrectAmount ()
+        {
+            MonopolyPlayer player = new MonopolyPlayer( "Peter" );
+            Assert.AreEqual( MonopolyPlayer.START_AMOUNT, player.Amount );
+        }
+    }
+}
